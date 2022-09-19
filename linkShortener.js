43895f45764b31b47b604f19f3877ea028d55335
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-09-19 06:11:17 +0200

    basic link rendering added

--- linkShortener.js
+++ linkShortener.js
@@ -1,4 +1,4 @@
-function shortenLink() {
+async function shortenLink() {
 
     const urlValue = document.querySelector("input").value;
     let ok;
@@ -6,11 +6,13 @@ function shortenLink() {
     try {
         //https://api.shrtco.de/v2/shorten?url=javatpoint.com/how-to-call-javascript-function-in-html
 
-        const returnedResponse = fetch("https://api.shrtco.de/v2/shorten?url=javatpoint.com/how-to-call-javascript-function-in-html").then((response) => response.json()).then((responseJSON) => {
+        const returnedResponse = fetch(`https://api.shrtco.de/v2/shorten?url=stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript`).then((response) => response.json()).then((responseJSON) => {
             console.log(responseJSON);
             console.log(responseJSON.result);
-        });
+            return createShortenedLinkCard(responseJSON.result.full_short_link);
+                });
         console.log(returnedResponse);
+
 
     } catch
         (exception) {
@@ -19,3 +21,49 @@ function shortenLink() {
 
 
 }
+
+async function copyShortenedLink() {
+
+    const shortenedLinkText = document.getElementsByClassName("shortened-link")[0].querySelector("p").innerText;
+    await navigator.clipboard.writeText(shortenedLinkText);
+
+}
+
+
+async function createShortenedLinkCard(shortenedUrl) {
+
+    const urlValue = document.querySelector("input").value;
+
+    const externalDiv = document.createElement("div");
+    externalDiv.className = "external-div";
+    const originLinkDiv = document.createElement("div");
+    originLinkDiv.className = "originLinkDiv";
+    const shortenedLink = document.createElement("div");
+    shortenedLink.className = "shortenedLink";
+
+
+    const originalLinkText = document.createElement("p");
+    originalLinkText.innerText = urlValue;
+    originLinkDiv.appendChild(originalLinkText);
+
+    const shortenedUrlText = document.createElement("p");
+    shortenedUrlText.innerText = shortenedUrl;
+    shortenedLink.appendChild(shortenedUrlText);
+
+    const copyButton = document.createElement("button");
+    copyButton.innerText = "Copy!";
+    copyButton.className = "copyButton";
+    copyButton.setAttribute("onClick", "javascript: copyShortenedLink();");
+
+
+    externalDiv.append(originLinkDiv, shortenedUrl, copyButton);
+
+    document.getElementsByClassName("link-shortener-text-area")[0].appendChild(externalDiv);
+
+    return document.contains(externalDiv) ?  1 : 2;
+
+}
+
+
+
+
